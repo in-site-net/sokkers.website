f6cf7ae232d79cfb8eb41a550c880b21e81abfec
in-site-net <infinitdata60@gmail.com>
2026-06-19 21:25:26 +0530

    Update TitleTrack.tsx

--- src/app/pages/TitleTrack.tsx
+++ src/app/pages/TitleTrack.tsx
@@ -2,7 +2,7 @@ export default function TitleTrack() {
   return (
     <div className="min-h-screen flex items-center justify-center p-6">
       <img
-        src="/public/music_sokkers.jpg"
+        src="/music_sokkers.jpg"
         alt="Title Track"
         className="max-w-full rounded-xl shadow-xl"
       />
