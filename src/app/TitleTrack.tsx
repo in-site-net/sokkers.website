a965659be4a065559ddf700472ca66e0a6956f09
in-site-net <infinitdata60@gmail.com>
2026-06-19 20:53:47 +0530

    Create TitleTrack.tsx

---
+++ src/app/TitleTrack.tsx
@@ -0,0 +1,11 @@
+export default function TitleTrack() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <img
+        src="/public/music_sokkers.jpg"
+        alt="Title Track"
+        className="max-w-full rounded-xl shadow-xl"
+      />
+    </div>
+  );
+}
