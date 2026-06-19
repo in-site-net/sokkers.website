12e487d4484394aedbf11b37460129d0b27af18f
in-site-net <infinitdata60@gmail.com>
2026-06-19 21:07:44 +0530

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
+
 import PasswordGate from "@/components/PasswordGate";
 import Navbar from "@/components/Navbar";
+
 import Home from "./pages/Home";
 import Homies from "./pages/Homies";
 import Memories from "./pages/Memories";
+import TitleTrack from "./pages/TitleTrack";
 
 function AnimatedRoutes() {
   const location = useLocation();
+
   return (
     <AnimatePresence mode="wait">
       <motion.div
@@ -21,6 +25,7 @@ function AnimatedRoutes() {
           <Route path="/" element={<Home />} />
           <Route path="/homies" element={<Homies />} />
           <Route path="/memories" element={<Memories />} />
+          <Route path="/title-track" element={<TitleTrack />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </motion.div>
@@ -30,7 +35,8 @@ function AnimatedRoutes() {
 
 export default function AppRouter() {
   const baseUrl = import.meta.env.BASE_URL;
-  const basename = baseUrl === "/" || baseUrl === "./" ? undefined : baseUrl;
+  const basename =
+    baseUrl === "/" || baseUrl === "./" ? undefined : baseUrl;
 
   return (
     <PasswordGate>
